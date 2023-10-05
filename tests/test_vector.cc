53845c44b141aeae09cbca7f8cc2b57990159a64
Tobias Leichtfried <tleichtfried@outlook.com>
2023-10-05 16:02:43 +0200

    basic matrix class

--- tests/test_vector.cc
+++ tests/test_vector.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <vector.h>
+#include "matrix.h"
 
 namespace bla = ASC_bla;
 
@@ -9,12 +10,15 @@ int main()
 {
   size_t n = 5;
   bla::Vector<double> x(n), y(n);
+  bla::Matrix<double> a(n,n), b(n,n);
 
   for (size_t i = 0; i < x.Size(); i++)
     {
       x(i) = i;
       y(i) = 10;
     }
+
+  a.Dump();
 
   bla::Vector<double> z = x+y;
   
