35033ad9e45ba7c4e0f19e5d6428d8df2b3df0a5
linux-03 <linus.s.knoll@gmail.com>
2023-10-12 15:10:18 +0200

    Reformatting

--- src/matrix.h
+++ src/matrix.h
@@ -16,13 +16,14 @@ namespace ASC_bla {
         size_t _width;
         size_t _height;
     public:
-        Matrix(size_t height,size_t width) : _data(width * height), _width{width}, _height{height} {}
-        Matrix(const Vector<T>& v) : _data(v.Size()), _width{1}, _height{v.Size()} {
-            for(size_t i=0;i<v.Size();i++)
-                (*this)(i,0)=v(i);
+        Matrix(size_t height, size_t width) : _data(width * height), _width{width}, _height{height} {}
+
+        Matrix(const Vector <T> &v) : _data(v.Size()), _width{1}, _height{v.Size()} {
+            for (size_t i = 0; i < v.Size(); i++)
+                (*this)(i, 0) = v(i);
         }
 
-        T& operator()(size_t row, size_t column) {
+        T &operator()(size_t row, size_t column) {
             if constexpr (ORD == Ordering::ColMajor) {
                 return _data[column * _height + row];
             } else {
@@ -30,7 +31,7 @@ namespace ASC_bla {
             }
         }
 
-        const T& operator()(size_t row, size_t column) const {
+        const T &operator()(size_t row, size_t column) const {
             if constexpr (ORD == Ordering::RowMajor) {
                 return _data[row * _width + column];
             } else {
@@ -42,65 +43,65 @@ namespace ASC_bla {
 
         size_t Height() const { return _height; }
 
-        Matrix<T> RowMultiply(size_t row, T c) const{
+        Matrix<T> RowMultiply(size_t row, T c) const {
             Matrix res = *this;
-            for(size_t i=0; i<Width();i++)
-                res(row,i)*=c;
+            for (size_t i = 0; i < Width(); i++)
+                res(row, i) *= c;
             return res;
         }
 
-        Matrix<T> RowAdd(size_t row,size_t dest) const{
+        Matrix<T> RowAdd(size_t row, size_t dest) const {
             Matrix res = *this;
-            for(size_t i=0; i<Width();i++)
-                res(dest,i)+=res(row,i);
+            for (size_t i = 0; i < Width(); i++)
+                res(dest, i) += res(row, i);
             return res;
         }
 
-        Matrix<T> RowMulAdd(size_t row,size_t dest, T c) const{
+        Matrix<T> RowMulAdd(size_t row, size_t dest, T c) const {
             Matrix res = *this;
             res = res.RowMultiply(row, c);
-            res = res.RowAdd(row,dest);
-            res = res.RowMultiply(row,1/c);
+            res = res.RowAdd(row, dest);
+            res = res.RowMultiply(row, 1 / c);
             return res;
         }
 
-        Matrix<T> RowSwap(size_t row,size_t dest) const{
+        Matrix<T> RowSwap(size_t row, size_t dest) const {
             Matrix res = *this;
-            for(size_t i=0; i<Width();i++)
-                std::swap(res(dest,i),res(row,i));
+            for (size_t i = 0; i < Width(); i++)
+                std::swap(res(dest, i), res(row, i));
             return res;
         }
 
-        
-        Matrix<T> EnsureNonzero(size_t row, size_t column) const{
+
+        Matrix<T> EnsureNonzero(size_t row, size_t column) const {
             Matrix res = *this;
-            size_t i=row;
-            for(; i<Height();i++)
-                if(res(i,column)!=0) break;
-            if(i==Height())
+            size_t i = row;
+            for (; i < Height(); i++)
+                if (res(i, column) != 0) break;
+            if (i == Height())
                 throw std::invalid_argument("Matrix singular");
-            return RowSwap(column,i);
+            return RowSwap(column, i);
         }
 
-        Matrix<T> Inverse() const{
-            if(this->Height() != this->Width())
+        Matrix<T> Inverse() const {
+            if (this->Height() != this->Width())
                 throw std::invalid_argument("Only square matrixes allowed");
-            Matrix<T> work{this->Height(),this->Height()*2};
-            for(size_t row=0; row < work.Height();row++){
-                for(size_t column=0; column < work.Height();column++){
-                    work(row,column)=(*this)(row,column);
-                    work(row,column+Height()) = (row==column) ? 1 : 0;
+            Matrix<T> work{this->Height(), this->Height() * 2};
+            for (size_t row = 0; row < work.Height(); row++) {
+                for (size_t column = 0; column < work.Height(); column++) {
+                    work(row, column) = (*this)(row, column);
+                    work(row, column + Height()) = (row == column) ? 1 : 0;
                 }
             }
             work.Dump();
-            for(size_t column=0; column < work.Height();column++){
-                work = work.EnsureNonzero(column,column);
+            for (size_t column = 0; column < work.Height(); column++) {
+                work = work.EnsureNonzero(column, column);
                 work.Dump();
-                work = work.RowMultiply(column,1/(work(column,column)));
+                work = work.RowMultiply(column, 1 / (work(column, column)));
                 work.Dump();
-                for(size_t row=0; row < work.Height();row++){
-                    if(row==column) continue;
-                    work = work.RowMulAdd(column,row,-work(row,column));
+                for (size_t row = 0; row < work.Height(); row++) {
+                    if (row == column) continue;
+                    work = work.RowMulAdd(column, row, -work(row, column));
                     work.Dump();
                 }
             }
@@ -126,52 +127,52 @@ namespace ASC_bla {
 
     template<typename T>
     Matrix<T> operator*(const Matrix<T> &a, const Matrix<T> &b) {
-        if(a.Width() != b.Height()){
+        if (a.Width() != b.Height()) {
             throw std::invalid_argument("Matrix dimension dont match");
         }
-        Matrix<T> result{a.Height(),b.Width()};
-        for(size_t row=0; row < result.Height();row++){
-            for(size_t column=0; column < result.Width();column++){
+        Matrix<T> result{a.Height(), b.Width()};
+        for (size_t row = 0; row < result.Height(); row++) {
+            for (size_t column = 0; column < result.Width(); column++) {
                 //Calculate C_row,column
-                T sum=0; //TODO: Zero element of T
-                for(size_t i=0;i<a.Width();i++){
-                    sum += a(row,i)*b(i,column);
+                T sum = 0; //TODO: Zero element of T
+                for (size_t i = 0; i < a.Width(); i++) {
+                    sum += a(row, i) * b(i, column);
                 }
-                result(row,column)=sum;
-            } 
+                result(row, column) = sum;
+            }
         }
         return result;
     }
 
 
     template<typename T>
-    Vector<T> operator*(const Matrix<T> &a, const Vector<T> &x) {
-        Matrix m = a*Matrix(x);
+    Vector <T> operator*(const Matrix<T> &a, const Vector <T> &x) {
+        Matrix m = a * Matrix(x);
         Vector<T> res{m.Height()};
-        for(size_t i=0;i<res.Size();i++)
-            res(i)=m(i,0);
+        for (size_t i = 0; i < res.Size(); i++)
+            res(i) = m(i, 0);
         return res;
     }
 
     template<typename T>
-    Vector<T> operator*(const Vector<T> &x, const Matrix<T> &a) {
+    Vector <T> operator*(const Vector <T> &x, const Matrix<T> &a) {
 
     }
 
     template<typename T>
     Matrix<T> operator*(T c, const Matrix<T> &a) {
         Matrix<T> result = a;
-        for(size_t row=0; row < result.Height();row++){
-            for(size_t column=0; column < result.Width();column++){
-                result(row,column)*=c;
-            } 
+        for (size_t row = 0; row < result.Height(); row++) {
+            for (size_t column = 0; column < result.Width(); column++) {
+                result(row, column) *= c;
+            }
         }
         return result;
     }
 
     template<typename T>
     Matrix<T> operator*(const Matrix<T> &a, T c) {
-        return c*a;
+        return c * a;
     }
 
 }
