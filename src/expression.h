5eea9068b35d28fdc8b1b5e385ed6952794a4b22
Tobias Leichtfried <tleichtfried@outlook.com>
2023-10-12 15:29:29 +0000

    matrix expr

--- src/expression.h
+++ src/expression.h
@@ -1,6 +1,6 @@
 #ifndef FILE_EXPRESSION_H
 #define FILE_EXPRESSION_H
-
+#include <exception>
 
 namespace ASC_bla {
 
@@ -21,12 +21,71 @@ namespace ASC_bla {
 
         size_t Width() const { return Upcast().Width(); }
         size_t Height() const { return Upcast().Height(); }
+        using ElemT = T;
 
         auto operator()(size_t row,size_t col) const { return Upcast()(row,col); }
     };
 
+    template<typename TA, typename TB>
+    class SumMatrixExpr : public MatrixExpr<SumMatrixExpr<TA, TB>>{
+        TA a_;
+        TB b_;
+    public:
+        SumMatrixExpr(TA a, TB b):a_(a),b_(b){}
+        auto operator()(size_t row, size_t col) const { return a_(row,col) + b_(row,col); }
+        size_t Width() const { return a_.Width(); }
+        size_t Height() const { return a_.Height(); }
+    };
 
+    template<typename TA, typename SCAL>
+    class ScaleMatrixExpr : public MatrixExpr<ScaleMatrixExpr<TA, SCAL>>{
+        TA _a;
+        SCAL _c;
+    public:
+        ScaleMatrixExpr(TA a, SCAL c):_a(a),_c{c}{}
+        auto operator()(size_t row, size_t col) const { return _a(row,col)*_c; }
+        size_t Width() const { return _a.Width(); }
+        size_t Height() const { return _a.Height(); }
+    };
 
+    template<typename TA, typename TB>
+    class MultiplyMatrixExpr : public MatrixExpr<MultiplyMatrixExpr<TA, TB>>{
+        TA a_;
+        TB b_;
+    public:
+        MultiplyMatrixExpr(TA a, TB b):a_(a),b_(b){}
+        auto operator()(size_t row, size_t col) const { 
+            if(a_.Width() != b_.Height()){
+                throw std::invalid_argument("Matrix dimension must match for multiplication");
+            }
+            typename TA::ElemT sum = 0; //TODO: Zero element of T
+            for (size_t i = 0; i < a_.Width(); i++) {
+                sum += a_(row, i) * b_(i, col);
+            }    
+        }
+        size_t Width() const { return b_.Width(); }
+        size_t Height() const { return a_.Height(); }
+    };
+
+    template<typename TM, typename TV>
+    class MatrixMulVecExpr : public VecExpr<MatrixMulVecExpr<TM, TV>> {
+        TM _m;
+        TV _v;
+    public:
+        MatrixMulVecExpr(TM m, TV v) : _m(m), _v(v) {}
+
+        auto operator()(size_t row) const { 
+            if(_m.Width() != _v.Size()){
+                throw std::invalid_argument("Matrix/Vector dimension must match for multiplication");
+            }
+            typename TM::ElemT sum = 0; //TODO: Zero element of T
+            for (size_t i = 0; i < _v.Size(); i++) {
+                sum += _m(row, i) * _v(i);
+            }   
+        }
+
+        size_t Size() const { return _m.Height(); }
+    };
 
     template<typename TA, typename TB>
     class SumVecExpr : public VecExpr<SumVecExpr<TA, TB>> {
@@ -72,7 +131,6 @@ namespace ASC_bla {
             ost << ", " << v(i);
         return ost;
     }
-
 }
 
 #endif
